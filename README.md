5606a8426ab9f5707e5e14a78af00d5b8e4b9614
Belen Andrade <belenandrade2000@gmail.com>
2023-05-10 00:19:47 -0700

    working on readme

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # employee-tracker
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- What was your motivation?
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+- What problem does it solve?
+- What did you learn?
+
+As companies grow in revenue, so do their number of employees, roles, managers, and departments. Every part of the business is intigrated with the others. Trying to decipher what employee has what role, who has a manger, and how many departments the company has can be confusing. I built this project to allow a user to input what information they want to know and the code will give them back a table of the requested information. This application is used to manage employee information. Keeping employee information organized and complete can be a difficult task to do by memory or by pen and paper. This application makes it easy to view all company departments, emmployees, and roles each employee can have. This application also allows a user to add a new department, new role, or new employee. Further, this application can also be used to update an employee's information. Through this project, I learned how to better use SELECT statements and overall how to query data. 
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+In order to use this application, you will need to have a couple npm libraries such as inquirer, and mysql2. In the terminal, simply write "npm i mysql2", "npm i inquirer@8.2.4" without the quotes. Ech line of code should be run separatly.
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative file path, add it to your README using the following syntax:
+
+![alt text](assets/images/screenshot.png)
+
+## Credits
+
+N/A
+
+## License
+
+Refer to Github repository. 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { connect } = require('http2');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
@@ -6,7 +7,7 @@ const db = mysql.createConnection(
     host: 'localhost',
     user: 'root',
     //switch back to hidden password
-    password: 'BeAr030317',
+    password: process.env.MYSQL_PASSWORD,
     database: 'employees_db'
   },
   console.log(`Connected to the employees_db database.`)
@@ -65,8 +66,17 @@ if (choices.options === 'Add Department') {
 function viewAllEmployees() {
 
     //fix join
-    const sql =`SELECT department_id,title, salary, department FROM roles`;
-    
+
+    //including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+    const sql = `
+    SELECT employees.id, employees.first_name, employees.last_name,
+      roles.title, departments.name AS department, roles.salary,
+      CONCAT(managers.first_name, ' ', managers.last_name) AS manager
+    FROM employees
+    JOIN roles ON employees.role_id = roles.id
+    JOIN departments ON roles.department_id = departments.id
+    LEFT JOIN employees AS managers ON employees.manager_id = managers.id
+  `;
     db.query(sql, (err,rows)=> {
         if (err) {
             console.log(err)
@@ -149,3 +159,99 @@ function addRole() {
     });
   });
 }
+
+
+//add employee
+
+function addEmployee() {
+  let roleNames;
+  db.query( `SELECT id, title FROM roles`, (err, rows)=> {
+    roleNames=rows
+  const roleChoices = roleNames.map(({id,title})=> ({
+    name:title, value:id
+  }))
+
+  let managerNames;
+  db.query( `SELECT manager_id FROM employees`, (err, rows)=> {
+    managerNames=rows
+  const managerChoices = managerNames.map(({manager_id})=> ({
+    value:manager_id
+  }))
+  
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'newFirstName',
+      message: 'What is the first name of the new employee',
+    },
+    {
+    type: 'input',
+    name: 'newLastName',
+    message: 'What is the last name of the new employee?',
+   },
+   {
+    type: 'list',
+    name: 'newEmployeeRole',
+    message: 'What role will the new employee have?',
+    choices: roleChoices,
+  
+   },
+   {
+    type: 'list',
+    name: 'newEmployeeManager',
+    message: 'Who will be the new manager of the new employee',
+    choices: managerChoices,
+  
+   },])
+   .then((answer) => {
+    let sql =`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+    db.query(sql, [answer.newFirstName,answer.newLastName, answer.newEmployeeRole, answer.newEmployeeManager], (error, response) => {
+      if (error) throw error;
+      viewAllEmployees ();
+              });
+    });
+  });
+})}
+
+//update employee role
+function updateEmployeeRole () {
+
+let employeeNames;
+db.query( 'SELECT CONCAT(first_name, " ", last_name) AS name FROM employees', (err, rows)=> {
+  employeeNames=rows
+const employeeChoices = employeeNames.map(({name})=> ({
+  name:name
+}))
+
+let roleNames;
+db.query( `SELECT id, title FROM roles`, (err, rows)=> {
+  roleNames=rows
+const roleChoices = roleNames.map(({id,title})=> ({
+  name:title, value:id
+}))
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'updateName',
+      message: 'What employee did you want to update?',
+      choices: employeeChoices,
+    },
+    {
+      type: 'list',
+      name: 'updateRole',
+      message: 'What is the employee\'s new role?',
+      choices: roleChoices,
+},])
+.then((answer) => {
+  let sql =`UPDATE employees SET employees.role_id = ? WHERE employees.id = ?`;
+  db.query(sql, [answer.updateName,answer.updateRole], (error, response) => {
+    if (error) throw error;
+    viewAllEmployees ();
+            });
+  });
+});
+
+})}
